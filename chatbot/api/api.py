31df48a47c7f466f9d72e01da196907e8baf8abe
Lucas <lucaskumara@hotmail.com>
2022-01-28 22:18:22 -0500

    Used to scraper to send data back to react

--- chatbot/api/api.py
+++ chatbot/api/api.py
@@ -1,12 +1,12 @@
 from flask import Flask
+from scraper import Scraper
 
 app = Flask(__name__)
+scraper = Scraper()
 
 @app.route('/api')
 def main():
-    return {
-        'content': 'This is a response from Flask!'
-    }
+    return scraper.get_clubs()
 
 if __name__ == '__main__':
     app.run(debug=True)
