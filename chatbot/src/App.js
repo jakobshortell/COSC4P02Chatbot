1d90b05a0e5755a89034c1e2e8403367592fbce9
Lucas <lucaskumara@hotmail.com>
2022-01-28 22:19:52 -0500

    Modified data to display

--- chatbot/src/App.js
+++ chatbot/src/App.js
@@ -20,7 +20,7 @@ function App() {
 		<div className="App">
 			<header className="App-header">
 				<img src={logo} className="App-logo" alt="logo" />
-				<p><code>{JSON.stringify(data)}</code></p>
+				<p><code>{ JSON.stringify(data[0]) }</code></p>
 				<a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">Learn React</a>
 			</header>
 		</div>
