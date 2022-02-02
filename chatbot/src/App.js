1e7a878fbce1f2206264c1aabdccee587add1139
Lucas <lucaskumara@hotmail.com>
2022-02-01 20:26:36 -0500

    Implemented new components + removed bot button

--- chatbot/src/App.js
+++ chatbot/src/App.js
@@ -1,95 +1,89 @@
 import React, { useRef, useState } from 'react';
-import { MessageContainer } from './components/messageContainer';
-import logo from './assets/logo.png';
 import './App.css';
+
+import { MessageContainer } from './components/messageContainer';
+import { Header } from './components/header';
+import { Input } from './components/input';
 
 const uuid = require('uuid');
 
 
 function App() {
 
-	const [messages, setMessages] = useState([]);
-	const inputContent = useRef();
+	const [messages, setMessages] = useState([
+		{
+			'author': 'bot',
+			'content': 'I\'m a Bot, ask me a question!',
+			'id': uuid.v4()
+		}
+	]);
+	const userInput = useRef();
+
+	function addMessage(message) {
+		setMessages((previousMessages) => {
+			return [...previousMessages, {
+				'author': message.author,
+				'content': message.content,
+				'id': message.id
+			}];
+		});
+		setTimeout(scroll, 200)
+	}
 
 	function addUserMessage() {
-
-		const message = inputContent.current.value
+		const message = userInput.current.value;
 
 		if (message !== '') {
-			setMessages((previousMessages) => {
-				return [...previousMessages, {
-					'author': 'User',
-					'content': message,
-					'id': uuid.v4()
-				}];
+			addMessage({
+				'author': 'user',
+				'content': message,
+				'id': uuid.v4()
 			});
-			inputContent.current.value = null;
-		}
 
+			userInput.current.value = null;
+			addBotMessage()
+		}
 	}
 
 	function addBotMessage() {
 
 		fetch('/api').then((response) => {
+
+			// Check response
 			if (response.ok) {
 				return response.json();
 			} else {
 				console.log(response.status)
 			}
+
 		}).then((data) => {
+
+			// Handle response
 			if (data !== {}) {
-				data = JSON.stringify(data).replace("{", "");
-				data = data.replace(/"/g,"");
-				data = data.replace("}", "");
-				setMessages((previousMessages) => {
-					return [...previousMessages, {
-						'author': 'Bot',
-						'content': data,
-						'id': uuid.v4()
-					}];
+				addMessage({
+					'author': 'bot',
+					'content': data.content,
+					'id': uuid.v4()
 				});
 			}
-		})
+
+		});
 
 	}
+
 	function scroll() {
-		var chatHistory = document.getElementById("scroll");
+		var chatHistory = document.getElementById('scroll');
 		chatHistory.scrollTop = chatHistory.scrollHeight;
-	
-		
 	}
-	function clickUser() {
-		addUserMessage()
-		setTimeout(scroll, 200)
-	}
-	function clickBot() {
-		addBotMessage()
-		setTimeout(scroll, 200)
-	
-	}
-	
 
 	return (
-		
-		<><div className="App">
-			<header className="App-header">
-				<img src={logo} className="Applogo" alt="logo" />
-				<div class="box" id="scroll">-----I'm a Bot, ask me a question-----
-					<MessageContainer messages={messages} />
-					<div class="box3"> </div>
-					</div>
-					<div class="chat">
-					<input class="input" ref={inputContent} type='text' />
-					<button type="button" class="button" onClick={clickUser} >Send</button>
-					</div>
-				<button type="button" class="button" onClick={clickBot}>TESTING: ADD BOT RESPONSE</button>
-			</header>
-		</div></>
-		
+		<div className='App'>
+			<Header />
+			<MessageContainer messages={messages} />
+			<Input userInput={userInput} onClick={addUserMessage} />
+		</div>
 	);
-}
 
-
-
+};
 
 export default App;
