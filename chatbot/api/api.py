86c7eb2fd7778792b67267dcd2daacb28e7d5be7
Lucas <lucaskumara@hotmail.com>
2022-02-03 17:29:06 -0500

    Removed unused code

--- chatbot/api/api.py
+++ chatbot/api/api.py
@@ -1,20 +1,21 @@
 from flask import Flask, request
-from random import seed
-from random import randint
-import sys
-import json
 
 app = Flask(__name__)
 
 
-@app.route('/api', methods=["POST","GET"])
+@app.route('/api', methods=['POST', 'GET'])
 def main():
     message = request.get_json()
     stringmessage = str(message)
-    print (stringmessage)
+
     if stringmessage=="None":
-        return {"content":"This is a test response. For testing purposes."}
-    return {"content":stringmessage.upper()}
+        return {
+            'content': 'This is a test response. For testing purposes.'
+        }
+
+    return {
+        'content': stringmessage.upper()
+    }
 
 
 if __name__ == '__main__':
