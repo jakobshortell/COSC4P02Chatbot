c103e3766560f1c44c372883cd4777fc0f6b57ec
Lucas <lucaskumara@hotmail.com>
2022-02-03 19:21:52 -0500

    Bot now provides club names

--- chatbot/api/api.py
+++ chatbot/api/api.py
@@ -9,9 +9,17 @@ scraper = Scraper()
 def main():
     request_data = request.get_json()
     user_message = request_data['userMessage']
-    return {
-        'content': scraper.get_clubs()
+
+    response = {
+        'content': 'I am sorry. I don\'t know what you are asking.'
     }
+
+    if 'club' in user_message.lower():
+        clubs = scraper.get_clubs()
+        club_names = [clubs[i]['name'] for i in clubs]
+        response['content'] = 'Here are all the clubs at Brock:\n\n' + ', '.join(club_names)
+
+    return response
 
 
 if __name__ == '__main__':
