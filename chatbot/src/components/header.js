1eb05b8aec78467509ec0057419b71bb1cb09b2a
Lucas <lucaskumara@hotmail.com>
2022-02-11 04:12:03 -0500

    Use new css modules

--- chatbot/src/components/header.js
+++ chatbot/src/components/header.js
@@ -1,14 +1,16 @@
 import React from 'react';
 
+import HeaderCSS from '../css/Header.module.css';
+
 import brockLogo from '../assets/brocklogo.jpeg';
 import settingsIcon from '../assets/setting.png';
 
 export function Header() {
 
 	return (
-		<header className='chatbot-header'>
-			<img className='logo' src={brockLogo} alt='logo' />
-			<img className='settings' src={settingsIcon} alt='settings' />
+		<header className={ HeaderCSS.chatbotHeader }>
+			<img className={ HeaderCSS.logo } src={brockLogo} alt='logo' />
+			<img className={ HeaderCSS.settings } src={settingsIcon} alt='settings' />
 		</header>
 	)
 
--- chatbot/src/components/input.js
+++ chatbot/src/components/input.js
@@ -1,14 +1,16 @@
 import React from 'react';
 
+import InputCSS from '../css/Input.module.css';
+
 import sendIcon from '../assets/send.png';
 
 export function Input({ userInput, onClick }) {
 
 	return (
-		<div className='input-container'>
-			<textarea className='input-field' type='text' ref={userInput} rows='1' placeholder='Type your question here' />
-			<button className='send-button' type='button' onClick={onClick}>
-				<img className='send-button-image' src={sendIcon} alt='sendButtonImage' />
+		<div className={ InputCSS.inputContainer }>
+			<textarea className={ InputCSS.inputField } type='text' ref={userInput} rows='1' placeholder='Type your question' />
+			<button className={ InputCSS.sendButton } type='button' onClick={onClick}>
+				<img className={ InputCSS.sendButtonImage } src={sendIcon} alt='sendButtonImage' />
 			</button>
 		</div>
 	)
