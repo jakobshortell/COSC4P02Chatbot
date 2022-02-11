4886ecc32fa791c9f3cb118253a3b0a64f45ab1d
Lucas <lucaskumara@hotmail.com>
2022-02-11 03:14:43 -0500

    Changed input bar

--- chatbot/src/components/input.js
+++ chatbot/src/components/input.js
@@ -6,7 +6,7 @@ export function Input({ userInput, onClick }) {
 
 	return (
 		<div className='input-container'>
-			<textarea className='input-field' type='text' ref={userInput} />
+			<textarea className='input-field' type='text' ref={userInput} rows='1' placeholder='Type your question here' />
 			<button className='send-button' type='button' onClick={onClick}>
 				<img className='send-button-image' src={sendIcon} alt='sendButtonImage' />
 			</button>
