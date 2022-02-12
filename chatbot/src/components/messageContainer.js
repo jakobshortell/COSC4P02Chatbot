b29b2cbd74389accc5959d2b3e9a0991589cb4fe
Lucas <lucaskumara@hotmail.com>
2022-02-12 16:30:15 -0500

    Moved code from index.css to a message container module

--- chatbot/src/components/messageContainer.js
+++ chatbot/src/components/messageContainer.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
+import MessageContainerCSS from '../css/MessageContainer.module.css';
+
 import { Message } from './Message';
 
 export function MessageContainer({ messages }) {
 
 	return (
-		<div className='message-container' id='scroll'>
-			<ul>
+		<div className={ MessageContainerCSS.messageContainer } >
+			<ul className={ MessageContainerCSS.scrollable } id='scroll'>
 				{
 					messages.map((message) => {
 						return <Message key={ message.id } author={ message.author } content={ message.content } />
