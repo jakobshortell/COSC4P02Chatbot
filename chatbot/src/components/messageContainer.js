ea20cc858f763d03eea6e08e02cf62be9fa9b68d
Lucas <lucaskumara@hotmail.com>
2022-02-12 22:15:01 -0500

    Added timestamp to message props

--- chatbot/src/components/messageContainer.js
+++ chatbot/src/components/messageContainer.js
@@ -7,11 +7,11 @@ import { Message } from './Message';
 export function MessageContainer({ messages }) {
 
 	return (
-		<div className={ MessageContainerCSS.messageContainer } >
+		<div className={ MessageContainerCSS.messageContainer }>
 			<ul className={ MessageContainerCSS.scrollable } id='scroll'>
 				{
 					messages.map((message) => {
-						return <Message key={ message.id } author={ message.author } content={ message.content } />
+						return <Message key={ message.id } author={ message.author } content={ message.content } time={ message.timestamp } />
 					})
 				}
 			</ul>
