6a26c7814a8f138a5dfbd1f70b7e2cfeba55af79
Lucas <lucaskumara@hotmail.com>
2022-02-13 01:10:17 -0500

    Made Brock logo link off to main site

--- chatbot/src/components/header.js
+++ chatbot/src/components/header.js
@@ -9,7 +9,9 @@ export function Header() {
 
 	return (
 		<header className={ HeaderCSS.chatbotHeader }>
-			<img className={ HeaderCSS.logo } src={ brockLogo } alt='logo' />
+			<a href='https://brocku.ca/' target='_blank' rel='noreferrer'>
+				<img className={ HeaderCSS.logo } src={ brockLogo } alt='logo' />
+			</a>
 			<img className={ HeaderCSS.settings } src={ settingsIcon } alt='settings' />
 		</header>
 	)
