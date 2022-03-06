14933a96ba2e05ed71d1610f62d894257d52b6f0
Lucas Kumara <lucaskumara@hotmail.com>
2022-03-05 22:16:33 -0500

    Began cleaning up api.py

--- chatbot/api/api.py
+++ chatbot/api/api.py
@@ -1,5 +1,4 @@
 from flask import Flask, request
-
 from scrapers.clubs import ClubScraper
 from scrapers.departments import DepartmentScraper
 from scrapers.important_dates import ImportantDatesScraper
@@ -8,7 +7,6 @@ from scrapers.programs import ProgramScraper
 from scrapers.exams import ExamScraper
 from scrapers.restaurant import RestaurantScraper
 from bot import Bot
-import random
 
 app = Flask(__name__)
 
@@ -23,29 +21,83 @@ scrapers = {
     'restaurant': RestaurantScraper()
 }
 
-
-
 @app.route('/api', methods=['POST', 'GET'])
 def main():
     '''The main enpoint for requests made to the chatbot.'''
+
+    # Obtain user message from the POST request
     request_data = request.get_json()
-    user_message = request_data['userMessage']
+    user_message = request_data['userMessage'].lower()
 
-    # Default response
-    response = {
-        'content': 'Error, something went wrong.'
+
+
+
+    # CLEAN UP HERE DOWN
+
+    response = {}
+    
+    msg = Bot.chat(user_message)
+    print(msg)
+
+    index = msg[0] # The index of data in the category
+    category = msg[1] # The category of data to get
+
+    # Access selection data using category and index
+    all_data = scrapers[category].get()
+    selection_data = all_data[index]
+
+    '''
+    The pieces of data below all have some form of standardized response and seem
+    to work well enough to be tested.
+
+    Example standard response
+
+    {
+        "table_name": TABLE_NAME,
+        "index": INDEX,
+        "associated_indexes": [INDEX, INDEX]  # Can be left empty if not pertinant
+        ...
     }
-    msg = Bot.chat(user_message.lower())
-    # Output an attribute of the first element in the response as a test
-    if 'club' in msg[1]:
-        clubs = scrapers['clubs'].get()
-        response['content'] = clubs[msg[0]]['name'] + ":\n" + clubs[msg[0]]['description'] + "\nEmail: " + clubs[msg[0]]['email']
 
-    elif 'department' in msg[1]:
-        departments = scrapers['departments'].get()
-        response['content'] = departments[msg[0]]['name'] + ":\n" + departments[msg[0]]['description'] + "\nLink:\t" + departments[msg[0]]['link'] + "\nSocial:\t" + departments[msg[0]]['social'] + "\nEmail:\t" + departments[msg[0]]['email'] + "\nPhone:\t" + departments[msg[0]]['extension']
+    '''
 
-    elif 'dates' in msg[1]:
+    if category == 'clubs':
+        # name = selection_data['name']
+        # description = selection_data['description']
+        # email = selection_data['email']
+
+        values = list(selection_data.values())
+        response['content'] = ', '.join(values)
+
+    elif category == 'departments':
+        # name = selection_data['name']
+        # description = selection_data['description']
+        # link = selection_data['link']
+        # social = selection_data['social']
+        # email = selection_data['email']
+        # extension = selection_data['extension']
+
+        values = list(selection_data.values())
+        response['content'] = ', '.join(values)
+
+    elif category == 'exams':
+        # course_code = selection_data['course_code']
+        # duration = selection_data['duration']
+        # day = selection_data['day']
+        # start = selection_data['start']
+        # end = selection_data['end']
+        # location = selection_data['location']
+
+        values = list(selection_data.values())
+        response['content'] = ', '.join(values)
+
+    '''
+    These categories below come with a different output from Bot.chat() or don't
+    appear to be working correctly so they haven't really been touched. The value
+    in bot.py was left unchanged for now.
+    '''
+
+    if 'dates' in msg[1]:
         dates = scrapers['dates'].get()
         response['content'] = dates[msg[0]]['occasion'] + " " + dates[msg[0]]['date']
 
@@ -60,10 +112,6 @@ def main():
             msg_temp = msg_temp + "\n" + restaurant[index]['name']
         response['content'] = msg[0] + msg_temp + "\nIs there one you would like more information on?"
 
-    elif 'exam' in msg[1]:
-        exams = scrapers['exams'].get()
-        response['content'] = exams[msg[0]]['course_code'] + " " + exams[msg[0]]['duration'] + " " + exams[msg[0]]['day'] + " " + exams[msg[0]]['start'] + " " + exams[msg[0]]['end'] + " " + exams[msg[0]]['location']
-
     elif 'programs' in msg[1]:
         programs = scrapers['programs'].get()
         response['content'] = programs[msg[0]]['name'] + "\n" + programs[msg[0]]['description'] + "\n" + programs[msg[0]]['prerequisites']
@@ -73,12 +121,11 @@ def main():
         msg_temp = ''
         for i in range(msg[3]):
             x = msg[0] - i
-            msg_temp = msg_temp + "\n" + courses[x]['duration'] + " " + courses[x]['day'] + " " + courses[x]['time'] + " " + courses[x]['type'] + " " + courses[x]['instructor']
+            msg_temp += "\n" + courses[x]['duration'] + " " + courses[x]['day'] + " " + courses[x]['time'] + " " + courses[x]['type'] + " " + courses[x]['instructor']
         response['content'] = courses[msg[0]]['course_code'] + " " + courses[msg[0]]['title'] + msg_temp
 
     else:
-        response['content'] = random.choice(msg)
-
+        response['content'] = 'Error, something went wrong.'
 
     return response
 
