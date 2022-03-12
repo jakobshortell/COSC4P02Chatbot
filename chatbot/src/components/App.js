fdf1567c9dc2c402bb94c638fafc0e3aa660cc68
Lucas <lucaskumara@hotmail.com>
2022-03-11 23:58:25 -0500

    Changed onClick to sendMessage for clarity

--- chatbot/src/components/App.js
+++ chatbot/src/components/App.js
@@ -1,27 +1,26 @@
-import { useRef, useState } from 'react';
+import { useRef, useState } from "react";
 
 // Styling
-import AppCSS from '../css/App.module.css';
+import AppCSS from "../css/App.module.css";
 
 // Components
-import Header from './Header';
-import Modal from './Modal';
-import MessageContainer from './MessageContainer';
-import Input from './Input';
+import Header from "./Header";
+import Modal from "./Modal";
+import MessageContainer from "./MessageContainer";
+import Input from "./Input";
 
-const uuid = require('uuid');
+const uuid = require("uuid");
 
 const App = () => {
-
 	const userInput = useRef();
 	const [messages, setMessages] = useState([
 		{
-			'author': 'bot',
-			'content': 'I\'m a Bot, ask me a question!',
-			'timestamp': getTime(),
-			'id': uuid.v4()
-		}
-	])
+			author: "bot",
+			content: "I'm a Bot, ask me a question!",
+			timestamp: getTime(),
+			id: uuid.v4(),
+		},
+	]);
 
 	function getTime() {
 		let date = new Date();
@@ -30,7 +29,7 @@ const App = () => {
 
 		// Correct minutes
 		if (minutes < 10) {
-			minutes = '0' + minutes.toString();
+			minutes = "0" + minutes.toString();
 		}
 
 		// Correct hours
@@ -43,84 +42,79 @@ const App = () => {
 			hours = hours - 12;
 			return `${hours}:${minutes} pm`;
 		}
-
 	}
 
 	function addMessage(message) {
 		setMessages((previousMessages) => {
-			return [...previousMessages, {
-				'author': message.author,
-				'content': message.content,
-				'timestamp': getTime(),
-				'id': message.id
-			}]
-		})
-		scroll()
+			return [
+				...previousMessages,
+				{
+					author: message.author,
+					content: message.content,
+					timestamp: getTime(),
+					id: message.id,
+				},
+			];
+		});
+		scroll();
 	}
 
 	async function addUserMessage() {
+		const message = userInput.current.value;
 
-		const message = userInput.current.value
-
-		if (message !== '') {
+		if (message !== "") {
 			addMessage({
-				'author': 'user',
-				'content': message,
-				'id': uuid.v4()
-			})
-			
-			userInput.current.value = null
-			addBotMessage(message)
-		}
+				author: "user",
+				content: message,
+				id: uuid.v4(),
+			});
 
+			userInput.current.value = null;
+			addBotMessage(message);
+		}
 	}
 
 	function addBotMessage(userMessage) {
-
-		fetch('/api', {
+		fetch("/api", {
 			method: "POST",
-			headers: {'Content-Type' : 'application/json'},
+			headers: { "Content-Type": "application/json" },
 			body: JSON.stringify({
-				'userMessage': userMessage
-			})
-		}).then((response) => {
-
-			// Check response
-			if (response.ok) {
-				return response.json();
-			} else {
-				console.log(response.status)
-			};
-
-		}).then((data) => {
-
-			// Create bot message
-			if (data !== {}) {
-				addMessage({
-					'author': 'bot',
-					'content': data.content,
-					'id': uuid.v4()
-				})
-			}
-
+				userMessage: userMessage,
+			}),
 		})
-
+			.then((response) => {
+				// Check response
+				if (response.ok) {
+					return response.json();
+				} else {
+					console.log(response.status);
+				}
+			})
+			.then((data) => {
+				// Create bot message
+				if (data !== {}) {
+					addMessage({
+						author: "bot",
+						content: data.content,
+						id: uuid.v4(),
+					});
+				}
+			});
 	}
 
 	function scroll() {
-		let chatHistory = document.getElementById('scroll')
-		chatHistory.scrollTop = chatHistory.scrollHeight
+		let chatHistory = document.getElementById("scroll");
+		chatHistory.scrollTop = chatHistory.scrollHeight;
 	}
 
 	return (
-		<div className={ AppCSS.app }>
+		<div className={AppCSS.app}>
 			<Header />
 			<Modal />
-			<MessageContainer messages={ messages } />
-			<Input userInput={ userInput } onClick={ addUserMessage } />
+			<MessageContainer messages={messages} />
+			<Input userInput={userInput} sendMessage={addUserMessage} />
 		</div>
-	)
-
-}
+	);
+};
 
 export default App;
