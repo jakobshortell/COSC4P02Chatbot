2281332a7d8837f411f5a3960c2831f27ebfd46a
Liam-Mckissock <lm09sr@brocku.ca>
2022-03-13 14:26:19 -0400

    Update brock_news.py

--- chatbot/api/scrapers/brock_news.py
+++ chatbot/api/scrapers/brock_news.py
@@ -5,10 +5,11 @@ class newsScraper:
     def get(self):
         rss = requests.get("https://brocku.ca/brock-news/feed/")
         title_list = rss.text
+        cnt = title_list.count('<title>')
         link_list = rss.text
         msg = 'Here is the latest Brock News:\n'
-        for x in range(8):
-            i = title_list.find('<title>') +7
+        for x in range(cnt):
+            i = title_list.find('<title>') + 7
             title_list = title_list[i::]
             i = title_list.find('</title>')
             title = title_list[:i:]
