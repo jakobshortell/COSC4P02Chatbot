3b719e53a27d077f8a695cbb6deb0e7244fe6f48
Liam-Mckissock <lm09sr@brocku.ca>
2022-03-29 15:44:49 -0400

    update

--- chatbot/api/bot.py
+++ chatbot/api/bot.py
@@ -22,7 +22,7 @@ with open("intents_" + language + "/intents.json") as file:
 with gzip.open('model_' + language + '/data', 'rb') as f:
     words, labels, training, output = pickle.load(f)
 
-dic = enchant.DictWithPWL("en_US", "customWords.txt")
+dic = enchant.DictWithPWL("en_US", "intents/customWords.txt")
 chkr = SpellChecker(dic)
 
 
@@ -55,8 +55,8 @@ def process_message(message):
     tf.compat.v1.reset_default_graph()
 
     net = tflearn.input_data(shape=[None, len(training[0])])
-    net = tflearn.fully_connected(net, 32)
-    net = tflearn.fully_connected(net, 32)
+    net = tflearn.fully_connected(net, 64)
+    net = tflearn.fully_connected(net, 64)
     net = tflearn.fully_connected(net, len(output[0]), activation="softmax")
     net = tflearn.regression(net)
 
@@ -70,19 +70,21 @@ def process_message(message):
     if results[results_index] > 0.1:
         for tag in data["intents"]:
             if tag['tag'] == tag_index:
-                table_name, index, associated_indexes, messages = tag['responses']
+                table_name, index, associated_indexes, messages, attributes = tag['responses']
                 msg = {
                     "table_name": None or table_name,
                     "index": None or index,
                     "associated_indexes": None or associated_indexes,
-                    "messages": None or messages
+                    "messages": None or messages,
+                    "attributes": None or attributes
                 }
     else:
         msg = {
             "table_name": None,
             "index": None,
             "associated_indexes": None,
-            "messages": ["I am sorry. I don\'t know  what you are asking."]
+            "messages": ["I am sorry. I don\'t know  what you are asking."],
+            "attributes": None
         }
 
     return msg
