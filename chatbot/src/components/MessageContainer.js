f574b2f0b3c5910e1cbdbb4857f6bcbbf52d7d31
Lucas Kumara <lucaskumara@hotmail.com>
2022-04-14 23:21:37 -0400

    Formatting

--- chatbot/src/components/MessageContainer.js
+++ chatbot/src/components/MessageContainer.js
@@ -1,27 +1,30 @@
-import React from 'react';
+import React from "react";
 
 // Styling
-import MessageContainerCSS from '../css/MessageContainer.module.css';
-import Dot from "./ActiveButton"
+import MessageContainerCSS from "../css/MessageContainer.module.css";
 
 // Components
-import Message from './Message';
+import Message from "./Message";
+import Dot from "./ActiveButton";
 
 const MessageContainer = ({ messages }) => {
-
 	return (
-		<div className={ MessageContainerCSS.messageContainer }>
-			<Dot/>
-			<ul className={ MessageContainerCSS.scrollable } id='scroll'>
-				{
-					messages.map((message) => {
-						return <Message key={ message.id } author={ message.author } content={ message.content } time={ message.timestamp } />
-					})
-				}
+		<div className={MessageContainerCSS.messageContainer}>
+			<Dot />
+			<ul className={MessageContainerCSS.scrollable} id="scroll">
+				{messages.map((message) => {
+					return (
+						<Message
+							key={message.id}
+							author={message.author}
+							content={message.content}
+							time={message.timestamp}
+						/>
+					);
+				})}
 			</ul>
 		</div>
-	)
-
-}
+	);
+};
 
 export default MessageContainer;
