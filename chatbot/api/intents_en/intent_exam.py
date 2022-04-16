e87b91753ece6277ce60a28f861a5b7600008a21
Liam-Mckissock <lm09sr@brocku.ca>
2022-04-16 18:01:00 -0400

    update

--- chatbot/api/intents_en/intent_exam.py
+++ chatbot/api/intents_en/intent_exam.py
@@ -8,8 +8,10 @@ class IntentExam:
     def generate(self, write):
         for i in exams:
             tag = exams[i]['course_code'] + " exams"
-            pattern = exams[i]['course_code'] + " exam", "can you when is the " + exams[i]['course_code'] + " exam", \
-                      "can you tell me where is the " + exams[i]['course_code'] + " exam"
+            pattern = exams[i]['course_code'] + " exam", "can you tell me when is the " + exams[i]['course_code'] + " exam", \
+                      "can you tell me where is the " + exams[i]['course_code'] + " exam", "what is the schedule for "\
+                      + exams[i]['course_code'] + " exam", "when is the " + exams[i]['course_code'] + " exam",\
+                      exams[i]['course_code'] + " exam schedule"
             response = 'exams', i, None, None, None
             intent = {
                 "tag": tag,
